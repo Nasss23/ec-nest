90255c3624b826feebe269702a7acc9fa4bb73f2
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2024-03-28 19:40:41 +0700

    add main

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const reflector = app.get(Reflector);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    // optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1'],
+  });
+  app.use(helmet());
+
+  await app.listen(configService.get<string>('PORT'));
+}
+bootstrap();
